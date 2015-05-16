b9116ac2af8791bfbdc7f2209e9b337f204e03f5
sedduret <sebastien.duret@msn.com>
2015-05-16 14:19:06 +0200

    init TortueBall file

---
+++ Tortuga/src/model/TortueBall.java
@@ -0,0 +1,16 @@
+package model;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Point;
+
+public class TortueBall extends Tortue{
+
+	@Override
+	public void draw(Graphics graph) {
+		Point p = new Point(this.getX(), this.getY());
+		graph.setColor(Color.YELLOW);
+		graph.drawOval(p.x, p.y, 10, 10);
+	}
+
+}
