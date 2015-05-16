d0113738994783a9a4d477942a4131835eca106c
sedduret <sebastien.duret@msn.com>
2015-05-16 14:27:15 +0200

    clear code

--- Tortuga/src/view/SimpleLogo.java
+++ Tortuga/src/view/SimpleLogo.java
@@ -11,22 +11,6 @@ import model.TortueAmelioree;
 import java.awt.event.*;
 import java.util.*;
 
-
-/*************************************************************************
-
-	Un petit Logo minimal qui devra etre ameliore par la suite
-
-				J. Ferber - 1999-2001
-
-				Cours de DESS TNI - Montpellier II
-
-	@version 2.0
-	@date 25/09/
-
-
-**************************************************************************/
-
-
 public class SimpleLogo extends JFrame implements Observer{
 	public static final Dimension VGAP = new Dimension(1,5);
 	public static final Dimension HGAP = new Dimension(5,1);
